ba46e682ac0b76fb5a9e0ae81a333ae88cc7b75c
BradfordCB <47071970+BradfordCB@users.noreply.github.com>
2025-12-11 16:37:29 -0700

    Update leglrpc.lua

--- units/Legion/Defenses/leglrpc.lua
+++ units/Legion/Defenses/leglrpc.lua
@@ -120,7 +120,7 @@ return {
 				explosiongenerator = "custom:genericshellexplosion-small",
 				gravityaffected = "true",
 				heightboostfactor = 8,
-				hightrajectory = 0.7,
+				hightrajectory = 1,
 				impulsefactor = 15.39,
 		        mygravity = 0.15,
 				leadbonus = 0,
