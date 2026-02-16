0f4e9ccfe676ab6d7fdd737e9dab1545b609911c
BradfordCB <47071970+BradfordCB@users.noreply.github.com>
2026-02-16 14:47:52 -0700

    Update mine_heavy.lua

--- weapons/mine_heavy.lua
+++ weapons/mine_heavy.lua
@@ -1,17 +1,17 @@
 return {
 	mine_heavy = {
-		areaofeffect = 730,
+		areaofeffect = 760,
 		craterboost = 0,
 		cratermult = 0,
-		edgeeffectiveness = 0.45,
+		edgeeffectiveness = 0.25,
 		explosiongenerator = "custom:genericunitexplosion-large-dirty",
-		impulsefactor = 4.8,
+		impulsefactor = 5.8,
 		name = "HeavyMine",
 		range = 480,
 		reloadtime = 3.6,
 		soundhit = "xplolrg3",
 		soundstart = "largegun",
-		weaponvelocity = 950,
+		weaponvelocity = 1250,
 		damage = {
 			default = 38004,
 			mines = 1,
