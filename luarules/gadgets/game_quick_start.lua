c5391a550f3ec29b6285e2910f8bdfad8a9258f9
BradfordCB <47071970+BradfordCB@users.noreply.github.com>
2026-06-12 15:05:13 -0600

    Update game_quick_start.lua

--- luarules/gadgets/game_quick_start.lua
+++ luarules/gadgets/game_quick_start.lua
@@ -34,7 +34,7 @@ local FACTORY_DISCOUNT_MULTIPLIER = 0.90 -- The factory discount will be the bud
 
 local QUICK_START_COST_ENERGY = 400      --will be deducted from commander's energy upon start.
 local QUICK_START_COST_METAL = 800       --will be deducted from commander's metal upon start.
-local READY_REFUNDABLE_BUDGET = 1800       -- Budget threshold when players are allowed to "ready" the game
+local READY_REFUNDABLE_BUDGET = 31800       -- Budget threshold when players are allowed to "ready" the game
 local quickStartAmountConfig = {
 	small = {
 		budget = 800,
@@ -49,7 +49,7 @@ local quickStartAmountConfig = {
 		traversabilityGridRange = 480
 	},
 	large = {
-		budget = 50000,
+		budget = 70000,
 		range = 2800,
 		baseGenerationRange = 2800,
 		traversabilityGridRange = 3300
